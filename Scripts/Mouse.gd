da5e127b408e2bb81e31ec3637e70687eba58b47
AlexDergach <dergachalex12@gmail.com>
2024-04-29 17:36:58 +0100

    Fixes

--- Scripts/Mouse.gd
+++ Scripts/Mouse.gd
@@ -3,7 +3,6 @@ extends CharacterBody3D
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 const DESTINATION_CHANGE_INTERVAL = 5.0 # Change destination every 5 seconds
-const FOOT_OFFSET = 0.1 # Maximum offset for foot motion
 
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
@@ -11,11 +10,6 @@ var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 var target_destination = Vector3.ZERO
 var destination_change_timer = 0.0
 var move = 1
-
-@onready var foot = $Foot
-@onready var foot_2 = $Foot2
-@onready var foot_3 = $Foot3
-@onready var foot_4 = $Foot4
 
 var foot_offset = 0.0
 
@@ -34,7 +28,6 @@ func _physics_process(delta):
 			velocity.x = direction.x * SPEED
 			velocity.z = direction.z * SPEED
 			
-			# Calculate the rotation angle based on the velocity vector
 			var rotation_angle = atan2(-velocity.z, velocity.x)
 			rotation.y = rotation_angle
 			
@@ -45,7 +38,7 @@ func _physics_process(delta):
 
 	move_and_slide()
 
-	# Update the timer and change destination if needed
+	# Pos Timer
 	destination_change_timer += delta
 	if destination_change_timer >= DESTINATION_CHANGE_INTERVAL:
 		set_random_destination()
