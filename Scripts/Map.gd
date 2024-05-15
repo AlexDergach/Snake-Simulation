fdd53a743872b6e0ef6f50f66c7ede0fa993a07d
AlexDergach <dergachalex12@gmail.com>
2024-05-15 22:30:13 +0100

    Adding sounds

--- Scripts/Map.gd
+++ Scripts/Map.gd
@@ -1,5 +1,8 @@
 extends Node3D
 
+const MOUSE = preload("res://Scene/Mouse.tscn")
+var mouse_instance
+var spawn_amount = 2
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -8,6 +11,16 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	
+	if spawn_amount > 0:
+		mouse_instance = MOUSE.instantiate()
+		add_child(mouse_instance)
+		spawn_amount -= 1
+		
+		mouse_instance.position.x = randf_range(10,-10)
+		mouse_instance.position.z = randf_range(10,-10)
+		mouse_instance.position.y = 1
+	
 	if Input.is_action_just_pressed("Esc"):
 		get_tree().quit()
 	pass
